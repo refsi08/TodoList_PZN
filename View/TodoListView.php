3f5ed92d3559b0e21feb1c32362e77c27a23e33f
RefDev <62316890+RefDev-00@users.noreply.github.com>
2023-07-31 09:34:52 +0700

    TodoList View

---
+++ View/TodoListView.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace View {
+
+    class TodoListView
+    {
+        function showTodoList(): void
+        {
+        }
+
+        function addTodoList(): void
+        {
+        }
+
+        function removeTodoList(): void
+        {
+        }
+    }
+}
